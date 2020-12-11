b4a9e03d50ac8f64278d2eb1c207ebc1a7121f9b
mikohan <angara99@gmail.com>
2020-12-11 14:19:58 +0300

    Toggle urls done

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,30 @@ interface IUrls {
   deleteUrl: string;
 }
 
-export const Urls: IUrls = {
-  fetchRowsUrl: 'http://localhost:8000/vasyainterface/workingrows/rows/',
+export const Urlss: IUrls = {
+  fetchRowsUrl: 'https://partshub.tk/vasyainterface/workingrows/rows/',
   angaraUrl:
     'http://angara77.com/admin33338/dataApi/vasyaInterfaceEndpoint.php?oneCId=',
   checkProductUrl: 'https://partshub.tk/vasyainterface/check',
-  deleteUrl: 'http://localhost:8000/vasyainterface/workingrows/rows',
+  deleteUrl: 'https://partshub.tk/vasyainterface/workingrows/rows',
 };
+
+export function makeUrls(location: string): IUrls {
+  if (location === 'local') {
+    return {
+      fetchRowsUrl: 'http://localhost:8000/vasyainterface/workingrows/rows/',
+      angaraUrl:
+        'http://angara77.com/admin33338/dataApi/vasyaInterfaceEndpoint.php?oneCId=',
+      checkProductUrl: 'https://partshub.tk/vasyainterface/check',
+      deleteUrl: 'http://localhost:8000/vasyainterface/workingrows/rows',
+    };
+  }
+  return {
+    fetchRowsUrl: 'https://partshub.tk/vasyainterface/workingrows/rows/',
+    angaraUrl:
+      'http://angara77.com/admin33338/dataApi/vasyaInterfaceEndpoint.php?oneCId=',
+    checkProductUrl: 'https://partshub.tk/vasyainterface/check',
+    deleteUrl: 'https://partshub.tk/vasyainterface/workingrows/rows',
+  };
+}
+export const Urls = makeUrls('local');
