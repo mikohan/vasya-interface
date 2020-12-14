584b04e7444cdf56e5d1d6447932c77daab430cf
mikohan <angara99@gmail.com>
2020-12-14 09:28:36 +0300

    Start implement table

---
+++ src/components/CheckPhotoTable.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+interface ICheckRow {
+  id: number;
+  one_c_id: number;
+  name: string;
+  brand: string;
+  cat_number: string;
+  have_photo: boolean;
+}
+interface IProps {
+  rows: ICheckRow[];
+}
+
+export default function BasicTable({ rows }: IProps) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">One_c_id</TableCell>
+            <TableCell align="right">Brand</TableCell>
+            <TableCell align="right">Cat Number</TableCell>
+            <TableCell align="right">Link to Site</TableCell>
+            <TableCell align="right">Link to Product</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
